b01045d6c29607ab0c6224df421cd1b90bee03e5
Seth <hullogh@gmail.com>
2024-04-13 02:04:20 +0800

    add balance account

--- programs/solana-anchor-deposit/src/lib.rs
+++ programs/solana-anchor-deposit/src/lib.rs
@@ -9,7 +9,35 @@ pub mod solana_anchor_deposit {
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
         Ok(())
     }
+
+    pub fn deposit(ctx: Context<Deposit>, amount: u64) -> Result<()> {
+        // let balance_account = &mut ctx.accounts.balance_account;
+        let balance_account = &mut ctx.accounts.balance_account;
+        let depositor = &ctx.accounts.depositor;
+
+        msg!("Depositing: {}", amount);
+        msg!("Depositor Account: {:?}", depositor.to_account_info().key);
+        msg!("Balance Account: {:?}", balance_account.to_account_info().key);
+
+        balance_account.balance += amount;
+        Ok(())
+    }
 }
 
 #[derive(Accounts)]
 pub struct Initialize {}
+
+#[derive(Accounts)]
+pub struct Deposit<'info> {
+    #[account(init, payer = depositor, space = 8 + 8)]
+    pub balance_account: Account<'info, BalanceAccount>,
+    #[account(mut)]
+    pub depositor: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[account]
+pub struct BalanceAccount {
+    pub balance: u64,
+}
+
--- tests/solana-anchor-deposit.ts
+++ tests/solana-anchor-deposit.ts
@@ -1,10 +1,12 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { SolanaAnchorDeposit } from "../target/types/solana_anchor_deposit";
+import { expect } from "chai";
 
 describe("solana-anchor-deposit", () => {
+  const provider = anchor.AnchorProvider.env();
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+  anchor.setProvider(provider);
 
   const program = anchor.workspace.SolanaAnchorDeposit as Program<SolanaAnchorDeposit>;
 
@@ -12,5 +14,39 @@ describe("solana-anchor-deposit", () => {
     // Add your test here.
     const tx = await program.methods.initialize().rpc();
     console.log("Your transaction signature", tx);
+  });
+
+  it('should deposit SOL into the programme balance account', async () => {
+    const depositor = anchor.web3.Keypair.generate();
+    console.log('Depositor:', depositor.publicKey.toBase58());
+
+    const balanceAccount = anchor.web3.Keypair.generate()
+    console.log('Balance account:', balanceAccount.publicKey.toBase58());
+
+    const connection = provider.connection;
+
+    await connection.confirmTransaction(
+      await connection.requestAirdrop(depositor.publicKey, 1000000000), 'confirmed',
+    );
+
+    console.log('Depositor balance:', await connection.getBalance(depositor.publicKey));
+
+
+    const tx = await program.methods.deposit(new anchor.BN(10000)).accounts({
+      balanceAccount: balanceAccount.publicKey,
+      depositor: depositor.publicKey,
+      systemProgram: anchor.web3.SystemProgram.programId,
+    })
+      .signers([depositor, balanceAccount])
+      .rpc();
+
+    console.log('Transaction:', tx);
+
+    await connection.confirmTransaction(tx, 'confirmed');
+
+    const balanceAccountData = await program.account.balanceAccount.fetch(balanceAccount.publicKey);
+    console.log(`Balance after deposit: ${balanceAccountData.balance.toString()}`);
+
+    expect(balanceAccountData.balance.toNumber()).to.equal(10000);
   });
 });
